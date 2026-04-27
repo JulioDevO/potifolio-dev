446fa297431f605e34326e793887c8fcd75c5a53
JulioDevO <julio.santos.developer@gmail.com>
2026-04-27 09:03:55 -0300

    feat: finaliza layout com secao de contato e rodape

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { HomeComponent } from './features/home/home.component'; 
 import { ProjetosComponent } from './features/projetos/projetos.component';
 import { ProcessosComponent } from './features/processos/processos.component';
+import { ContatoComponent } from "./features/contato/contato.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HomeComponent, ProjetosComponent, ProcessosComponent],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, ProjetosComponent, ProcessosComponent, ContatoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
